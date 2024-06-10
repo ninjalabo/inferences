51d3cdd9e289d7946e0e4e0badbb027a735762b7
hd702 <haru.doyu@gmail.com>
2024-06-10 12:40:54 +0300

    update script to run inferences
    output file is changed from  to  + instead of taking paths to all data, it will take path to data dir

--- run.sh
+++ run.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
 if [ $# -lt 4 ]; then
-  echo "Usage: $0 <image_name> <inference> <model_path> <data_path...>"
+  echo "Usage: $0 <inference> <model_path> <data_path...>"
   exit 1
 fi
 
-image_name=$1
-inference=$2
-model_path=$3
-shift 3
+inference=$1
+model_path=$2
+shift 2
 data_paths="$@"
 
 if [ ${inference##*.} = py ]; then
@@ -16,7 +15,6 @@ if [ ${inference##*.} = py ]; then
 else
   command=$inference
 fi
-
 start_time=$(date +%s)
 accuracy=$($command $model_path $data_paths)
 end_time=$(date +%s)
@@ -28,7 +26,7 @@ else
   model_size=$(stat -c%s $model_path)
 fi
 
-# Update output.json file for the specified image
+# Update runtime_info.json
 temp_file=$(mktemp)
-jq "map(if .image == \"$image_name\" then . + {results: {accuracy: $accuracy, size: $model_size, speed: $speed}} else . end)" md/output.json > "$temp_file"
-mv "$temp_file" md/output.json
+jq ".results = {accuracy: $accuracy, size: $model_size, speed: $speed}" md/runtime_info.json > "$temp_file"
+mv "$temp_file" md/runtime_info.json
